46b17b8d002a732f93e8e851e830f9d78b922b43
Thomas G. Wolf <github@wolfhouse.me>
2024-03-22 19:36:11 -0600

    Enabled unattended updates to the debian base 12 template

--- packer-vm-templates/proxmox/debian-12-base/files/provision.sh
+++ packer-vm-templates/proxmox/debian-12-base/files/provision.sh
@@ -1,11 +1,21 @@
 
+# General preparation for the template
 sudo -n rm /etc/ssh/ssh_host_*
 sudo -n truncate -s 0 /etc/machine-id
+
+# Prepare apt for the template
 sudo -n apt -y autoremove --purge
 sudo -n apt -y clean
 sudo -n apt -y autoclean
+
+# Prepare cloud-init for the template
 sudo -n cloud-init clean
 sudo -n rm -f /etc/cloud/cloud.cfg.d/subiquity-disable-cloudinit-networking.cfg
-sudo -n mv /tmp/99-pve.cfg /etc/cloud/cloud.cfg.d/99-pve.cfg
-sudo -n mv /tmp/cloud.cfg  /etc/cloud/cloud.cfg
+sudo -n mv /tmp/99-pve.cfg            /etc/cloud/cloud.cfg.d/99-pve.cfg
+sudo -n mv /tmp/cloud.cfg             /etc/cloud/cloud.cfg
+
+# Move misc configuration into their respective locations
+sudo -n mv /tmp/50unattended-upgrades /etc/apt/apt.conf.d
+sudo -n mv /tmp/20auto-upgrades       /etc/apt/apt.conf.d
+
 sudo -n sync
