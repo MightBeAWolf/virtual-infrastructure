cd3541130262f7ce17bf4431b5569a3e30c1096b
Thomas G. Wolf <github@wolfhouse.me>
2024-03-23 13:44:32 -0600

    Initialized terraform, still needs work

--- k3s-proxmox-cluster/k3s-cluster.tf
+++ k3s-proxmox-cluster/k3s-cluster.tf
@@ -1,12 +1,86 @@
+variable "pm_api_url" {
+  description = "The Proxmox API URL"
+  type        = string
+}
+
+variable "pm_user" {
+  description = "The Proxmox user for API access"
+  type        = string
+}
+
+variable "pm_api_token_id" {
+  description = "The Proxmox API token created for a specific user."
+  type        = string
+}
+
+variable "pm_api_token_secret" {
+  description = "The password for the Proxmox API user"
+  type        = string
+  sensitive   = true
+}
+
+variable "pm_tls_insecure" {
+  description = "Disable TLS verification for Proxmox API"
+  type        = bool
+  default     = true
+}
+
+variable "node_count" {
+  description = "The number of K3s nodes to create"
+  type        = number
+  default     = 3
+}
+
+variable "vm_template_name" {
+  description = "The name of the VM template to clone for K3s nodes"
+  type        = string
+  default     = "debian-12-base"
+}
+
+variable "vm_resource_settings" {
+  description = "Resource settings for each K3s node VM"
+  type = object({
+    cores   = number
+    memory  = number
+    storage = string
+  })
+  default = {
+    cores   = 2
+    memory  = 2048
+    storage = "local-lvm:20G"
+  }
+}
+
+
 terraform {
   required_providers {
     proxmox = {
       source  = "telmate/proxmox"
-      version = "<version tag>"
+      version = "3.0.1-rc1"
     }
   }
 }
 
 provider "proxmox" {
-  # Configuration options
+  pm_api_url          = var.pm_api_url
+  pm_user             = var.pm_user
+  pm_api_token_id     = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret
+  pm_tls_insecure     = var.pm_tls_insecure
+  pm_debug            = true
 }
+
+resource "proxmox_vm_qemu" "k3s_node" {
+  count = var.node_count
+  name  = "k3s-node-${count.index}"
+  clone = var.vm_template_name
+  cores = var.vm_resource_settings.cores
+  memory = var.vm_resource_settings.memory
+  disk = var.vm_resource_settings.storage
+  // Include additional configurations as needed
+
+  provisioner "remote-exec" {
+    // Commands to install K3s go here
+  }
+}
+
--- k3s-proxmox-cluster/run.sh
+++ k3s-proxmox-cluster/run.sh
@@ -6,64 +6,50 @@ PROXMOX_TEMPLATE_ID="8000"
 
 # Secrets
 ONEPASSWORD_VAULT="Local Cluster"
-export PROXMOX_URL="op://${ONEPASSWORD_VAULT:?}/Proxmox - Terraform Provider/hostname"
-export PROXMOX_TOKEN="op://${ONEPASSWORD_VAULT:?}/Proxmox - Terraform Provider/credential"
-export PROXMOX_USERNAME="op://${ONEPASSWORD_VAULT:?}/Proxmox - Terraform Provider/tokenid"
-
-# export PKR_VAR_ssh_fullname="op://${ONEPASSWORD_VAULT:?}/Local Cluster Template User/fullname"
-# export PKR_VAR_ssh_username="op://${ONEPASSWORD_VAULT:?}/Local Cluster Template User/username"
-# export PKR_VAR_ssh_password="op://${ONEPASSWORD_VAULT:?}/Local Cluster Template User/password"
+export TF_VAR_pm_api_url="op://${ONEPASSWORD_VAULT:?}/Proxmox - Terraform Provider/hostname"
+export TF_VAR_pm_user="op://${ONEPASSWORD_VAULT:?}/Proxmox - Terraform Provider/username"
+export TF_VAR_pm_api_token_id="op://${ONEPASSWORD_VAULT:?}/Proxmox - Terraform Provider/tokenid"
+export TF_VAR_pm_api_token_secret="op://${ONEPASSWORD_VAULT:?}/Proxmox - Terraform Provider/credential"
 
 # The first argument is the target, the rest are options for terraform
 TARGET=$1
 shift # Remove the first argument, leaving any additional arguments
 
-# terraform template file
-TERRAFORM_CONF="k3s-cluster.tf"
-
-# Function for terraform verify
-terraform-format() {
-  op run -- terraform validate \
-    "$@" \
-    "${TERRAFORM_CONF:?}"
-}
-
 # Function for terraform verify
 terraform-validate() {
-  op run -- terraform validate \
-    "$@" \
-    "${TERRAFORM_CONF:?}"
+  op run -- terraform validate "$@"
 }
 
 # Function for terraform verify
 terraform-init() {
-  op run -- terraform validate \
-    "$@" \
-    "${TERRAFORM_CONF:?}"
+  op run -- terraform init "$@"
+}
+
+# Function for terraform verify
+terraform-plan() {
+  op run -- terraform plan "$@"
 }
 
 # Function for terraform build
 terraform_apply() {
-  op run -- terraform apply \
-    "$@" \
-    "${TERRAFORM_CONF:?}"
+  op run -- terraform apply "$@"
 }
 
 # Usage function to display help for the script
 usage() {
-  echo "Usage: $0 {validate|verify|format|init|apply} [options]"
+  echo "Usage: $0 {validate|verify|init|plan|apply} [options]"
   echo "Mimics the functionality of a Makefile for Terraform."
 }
 # Main case switch for the script
 case "$TARGET" in
-  format)
-    terraform-format "$@"
-  ;;
   verify|validate)
     terraform-validate "$@"
   ;;
   init)
     terraform-init "$@"
+  ;;
+  plan)
+    terraform-plan "$@"
   ;;
   apply)
     terraform_build "$@"
