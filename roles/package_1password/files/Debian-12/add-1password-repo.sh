2bd4486f95f1f828655efe0ab45a5dca66e33bf9
Thomas G Wolf <gwolf@silicontechnologiesinc.com>
2024-08-05 21:40:55 -0600

    Got the backupsys setup to the point of authorizing keys

--- roles/package_1password/files/Debian-12/add-1password-repo.sh
+++ roles/package_1password/files/Debian-12/add-1password-repo.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
-if [[ ! -f /usr/share/keyrings/1password-archive-keyring.gpg ]]; then
 curl -sS https://downloads.1password.com/linux/keys/1password.asc \
-  | gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
-fi
+  | gpg --dearmor --batch --yes --output /usr/share/keyrings/1password-archive-keyring.gpg
 
-if [[ ! -f /etc/debsig/policies/AC2D62742012EA22/1password.pol ]]; then
 mkdir -p /etc/debsig/policies/AC2D62742012EA22
 curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol \
   | tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-fi
 
-if [[ ! -f /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg ]]; then
 mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-  | gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-fi
+curl -sS https://downloads.1password.com/linux/keys/1password.asc \
+  | gpg --dearmor --batch --yes --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
 
-cat > /etc/apt/sources.list.d/1password.list << HEREDOC
-deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main"
-HEREDOC
-
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" > /etc/apt/sources.list.d/1password.list
